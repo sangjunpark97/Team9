91b1e99a69528f4370f46e03739ad11c074d9a37
sangjunpark97 <djg04138@gmail.com>
2023-10-19 22:02:38 +0900

    test

--- app/src/main/java/com/example/team9/MainActivity.kt
+++ app/src/main/java/com/example/team9/MainActivity.kt
@@ -7,7 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Test 추가 확인 ㅇㄴㄹㅇㄴㄹ
+        // Clone Test
 
         setContentView(R.layout.activity_main)
     }
