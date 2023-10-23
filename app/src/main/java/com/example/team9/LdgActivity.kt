fb76759690bc344327352760f0306591c89645d5
hani1875 <hani1875@naver.com>
2023-10-24 04:07:26 +0900

    2023-10-23

---
+++ app/src/main/java/com/example/team9/LdgActivity.kt
@@ -0,0 +1,12 @@
+package com.example.team9
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LdgActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.ldg)
+    }
+}
