9408112761350c54e1838a66eebd903491b884e3
jwaminseok <darth0324@naver.com>
2023-10-24 10:05:19 +0900

    jwajwa

--- app/src/main/java/com/example/team9/todolistactivity.kt
+++ app/src/main/java/com/example/team9/todolistactivity.kt
@@ -1,16 +1,22 @@
 package com.example.team9
 
-import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 import com.example.team9.databinding.ActivityMainBinding
 
-class LdgActivity : AppCompatActivity() {
-    lateinit var binding : ActivityMainBinding   //지연 초기화
+class todolistactivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_todolistactivity)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+
+
+
+
 
     }
 }
