50894df27704222e4f8ae1002ca78555fbd13d46
sangjunpark97 <djg04138@gmail.com>
2023-10-24 11:08:40 +0900

    test

--- app/src/main/java/com/example/team9/MainActivity.kt
+++ app/src/main/java/com/example/team9/MainActivity.kt
@@ -7,7 +7,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Stash
+        //sdfdsdf
+        // dfsd
 
         setContentView(R.layout.activity_main)
     }
