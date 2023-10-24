09e709ded4175dd6a474c80a9883431da014f5e3
hani1875 <hani1875@naver.com>
2023-10-24 10:08:18 +0900

    2023-10-24

--- app/src/main/java/com/example/team9/LdgActivity.kt
+++ app/src/main/java/com/example/team9/LdgActivity.kt
@@ -4,12 +4,13 @@ import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.team9.databinding.ActivityMainBinding
+import com.example.team9.databinding.LdgBinding
 
 class LdgActivity : AppCompatActivity() {
-    lateinit var binding : ActivityMainBinding
+    lateinit var binding : LdgBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = LdgBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
     }
