ec5c9d4ab813d318c088f90e73cec3454bdff5b0
jwaminseok <darth0324@naver.com>
2023-10-24 11:12:15 +0900

    jwajwa

--- app/src/main/java/com/example/team9/MainActivity.kt
+++ app/src/main/java/com/example/team9/MainActivity.kt
@@ -8,10 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         //sdfdsdf
-        // dfsd
-        // test
-
-        //ㄴㅇㄹㄴㅇㄹ
 
         setContentView(R.layout.activity_main)
     }
