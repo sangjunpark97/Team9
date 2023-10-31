f2b5724e8993e08ffdaaddca184528e69b04f71b
hani1875 <hani1875@naver.com>
2023-10-31 13:32:42 +0900

    2023-10-31

--- app/src/main/java/com/example/team9/MainActivity.kt
+++ app/src/main/java/com/example/team9/MainActivity.kt
@@ -2,17 +2,15 @@ package com.example.team9
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.team9.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        //sdfdsdf
-        // dfsd
-        // test123
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        //ㄴㅇㄹㄴㅇㄹㅂㅈㄷㅂㅈㄷ
 
-        setContentView(R.layout.activity_main)
     }
 }
