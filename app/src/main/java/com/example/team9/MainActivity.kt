920ebe4e2399b7b8fd9707f81825bbdc4140948b
sangjunpark97 <djg04138@gmail.com>
2023-11-06 21:19:17 +0900

    bottom nav 연동

--- app/src/main/java/com/example/team9/MainActivity.kt
+++ app/src/main/java/com/example/team9/MainActivity.kt
@@ -3,29 +3,27 @@ package com.example.team9
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.team9.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
+    lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val navController = binding.frgNav.getFragment<NavHostFragment>().navController
+        setupActionBarWithNavController(navController)
+        binding.bottomNav.setupWithNavController(navController)
         setContentView(binding.root)
-
-        fun replaceFragment(frag: Fragment) {
-            supportFragmentManager.beginTransaction().let {
-                it.replace(binding.recSchedule.id, Fragment())
-                it.commit()
-            }
-        }
-
-
-        replaceFragment(TodoListFragment())
-
-
-
-
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = binding.frgNav.getFragment<NavHostFragment>().navController
+
+        return navController.navigateUp() || super.onSupportNavigateUp()
+     }
 }
