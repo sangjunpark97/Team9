4c7b6db8b9c854b44fa65e75c0d3f3a6987d6660
sangjunpark97 <djg04138@gmail.com>
2023-11-07 09:38:12 +0900

    Subject 수정

--- app/src/main/java/com/example/team9/CTimeTable.kt
+++ app/src/main/java/com/example/team9/CTimeTable.kt
@@ -1,13 +1,32 @@
 package com.example.team9
 
+data class CToDo(var what: String){
+    var deadline: String? = null
+}
 
-data class Subject(val name: String, val day: String, val place: String, val startTime: String, val endTime: String)
+data class Subject(val name: String) {
+    var type: String? = null
+    var credit: Int = -1
+    var grade: Int = -1
+    var day: String? = null
+    var place: String? = null
+    var startTime: String? = null
+    var endTime: String? = null
+    var memo: String? = null
+
+    val toDoList = mutableListOf<String>()
+}
 
 class TimeTable {
-    val subjects = mutableListOf<Subject>()  // 수업 목록을 저장할 리스트
+    val subjects = mutableListOf<Subject>()
 
-    fun addSubject(name: String, day: String, place: String, startTime: String, endTime: String) {
-        val subject = Subject(name, day, place, startTime, endTime)
+    fun addSubject(name: String, day: String, place: String, startTime: String, endTime: String, memo: String? = null) {
+        val subject = Subject(name)
+        subject.day = day
+        subject.place = place
+        subject.startTime = startTime
+        subject.endTime = endTime
+        subject.memo = memo
         subjects.add(subject)
     }
 
@@ -15,6 +34,39 @@ class TimeTable {
         println("Timetable:")
         for (subject in subjects) {
             println("${subject.name}: ${subject.day}, ${subject.place}, ${subject.startTime} - ${subject.endTime}")
+            if (subject.memo != null) {
+                println("Memo: ${subject.memo}")
+            }
+            println("TO-DO List:")
+            for (toDoItem in subject.toDoList) {
+                println("what? : ${toDoItem.what}")
+                if (toDoItem.deadline != null) {
+                    println("deadline : ${toDoItem.deadline}")
+            }
         }
     }
 }
+
+//참고
+
+/*
+
+fun main() {
+    val myTimetable = TimeTable()
+
+    myTimetable.addSubject("Math", "Monday", "Room A", "09:00 AM", "10:30 AM", "Review chapter 3")
+    myTimetable.addSubject("History", "Tuesday", "Room B", "02:00 PM", "03:30 PM", "Bring textbook")
+    myTimetable.addSubject("English", "Wednesday", "Room C", "11:30 AM", "01:00 PM")
+
+    myTimetable.subjects[0].toDoList.add("Complete Math homework")
+    myTimetable.subjects[0].toDoList.add("Study for Math test")
+
+    myTimetable.subjects[1].toDoList.add("Read history chapter")
+
+    myTimetable.subjects[2].toDoList.add("Prepare for English presentation")
+    myTimetable.subjects[2].toDoList.add("Review grammar")
+
+    myTimetable.displayTimeTable()
+}
+
+ */
